2e6168f6d31dc0f3fd537aaf7425e220a28edf1d
simonhangarita <101430109+simonhangarita@users.noreply.github.com>
2024-11-28 09:38:19 -0500

    changes in vite.config.ts

--- frontend/app/vite-project/vite.config.ts
+++ frontend/app/vite-project/vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ["@mui/material", "@mui/system", "@mui/icons-material"],
+  },
+  build: {
+    target: "esnext", // Ensures modern syntax
+    chunkSizeWarningLimit: 1000, // Adjust chunk warning limit
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Separate large libraries into chunks
+          mui: ["@mui/material", "@mui/system", "@mui/icons-material"],
+        },
+      },
+    },
+  },
+});
